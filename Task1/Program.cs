6e67ec13779432580cf93f7b69a66f5f171c7e90
edochik <edoch@yandex.ru>
2022-04-21 13:29:57 +0300

    do Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = new Random().Next(10, 100);
+int i = 10;
+int result = 0;
+while (i-1 < n)
+{
+    i = i + 10;
+    result = result + 10;
+}
+int a = n - result;
+int b = result / 10;
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
