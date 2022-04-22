a9d49fe2b4e583f8765cc13d8ab62df9726b5c1d
edochik <edoch@yandex.ru>
2022-04-22 18:03:31 +0300

    modified Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,21 +1,11 @@
-﻿double N1 = 34;
-double N2 = 5;
-int i = 10;
-int result = 0;
-double a = (N1 / N2);
-if (a % 1 == 0) // делим на 1 если остаток не равен нулю, то число не целое(google)
+﻿int a = 34;
+int b = 5;
+if (a % b == 0)
 {
-    Console.WriteLine("Кратно");
+    Console.WriteLine("кратно");
 }
 else
 {
-    while (i-2 < N1)
-    {
-        result = result + 10;
-        i = i + 10;
-        Console.WriteLine(result);
-    }
-    double b = N1 - result;
-    Console.WriteLine("не кратн, остаток " + b);
+    int c = a % 10;
+    Console.WriteLine("не кратно, остаток " + c);
 }
-// с целыми десятками не работает 30 будет выводить остаток 10
