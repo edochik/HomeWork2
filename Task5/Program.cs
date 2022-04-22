07563bc3379979c82215136ecb0590ebaf5a34be
edochik <edoch@yandex.ru>
2022-04-22 17:24:08 +0300

    add task5

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 25;
+int b = 5;
+if (a > b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+if (a < b)
+{
+    if (b % a == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
