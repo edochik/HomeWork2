678798d22fe3e6945e7f866aacb9c2fee1c1052c
edochik <edoch@yandex.ru>
2022-04-24 15:56:19 +0300

    modified Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,25 +1,5 @@
-﻿int a = 35767;
-if (a < 100)
-{
-    Console.WriteLine("третьей цифры нет");
-}
-if (a < 1000)
-{
-    int b = a % 10;
-    Console.WriteLine(b);
-}
-if (a < 10000)
-{
-    int c = a % 100;
-    int d = c % 10;
-    int e = (c - d) / 10;
-    Console.WriteLine(e);
-}
-if (a < 100000)
-{
-    int c = a % 1000;
-    int b = c % 100;
-    int e = b % 10;
-    int f = (c-(b - e)) / 100;
-    Console.WriteLine(f);
-}
+﻿int n = -9118;
+int a = (n *(-1)) % 100;
+int b = a % 10;
+int c = (a - b) / 10;
+Console.WriteLine(c);
--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,9 +1,26 @@
-﻿int a = 5;
-if (a == 6 ^ a == 7)
+﻿int a = 35698;
+if (a < 100)
 {
-    Console.WriteLine("да");
+    Console.WriteLine("третьей цифры нет");
 }
-else
+if (a < 1000)
 {
-    Console.WriteLine("нет");
+    int b = a % 10;
+    Console.WriteLine(b);
 }
+if (a < 10000)
+{
+    int c = a % 100;
+    int d = c % 10;
+    int e = (c - d) / 10;
+    Console.WriteLine(e);
+}
+if (a < 100000)
+{
+    int c = a % 1000;
+    int b = c % 100;
+    int e = b % 10;
+    int f = (c-(b - e)) / 100;
+    Console.WriteLine(f);
+}
+
